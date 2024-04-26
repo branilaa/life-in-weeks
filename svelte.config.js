9950d4af28bfa4e457df91496b62516ce382d90e
branilaa <stefan.branila55@gmail.com>
2024-04-27 00:10:45 +0200

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from '@sveltejs/adapter-auto';
+
+const config = {
+	kit: {
+		adapter: adapter(),
+
+		env: {
+			dir: './'
+		},
+
+		alias: {
+			$db: './src/lib/server/db',
+			$components: './src/lib/components',
+		}
+		}
+}
+
+export default config;
